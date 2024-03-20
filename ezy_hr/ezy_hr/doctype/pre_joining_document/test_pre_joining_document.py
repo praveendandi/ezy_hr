ac02bcb758d4041ff40b5a464bc92f4600f6a7e0
girishjaiswal1993 <girish.jaiswal@caratred.com>
2024-03-20 15:17:02 +0530

    add custom field in job app and custom doctype

---
+++ ezy_hr/ezy_hr/doctype/pre_joining_document/test_pre_joining_document.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ganu Reddy and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPrejoiningdocument(FrappeTestCase):
+	pass
