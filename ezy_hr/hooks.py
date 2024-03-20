9f74e57821d34149a0e1c11dcce4de948b21346e
girishjaiswal1993 <girish.jaiswal@caratred.com>
2024-03-20 15:45:23 +0530

    initial changes

--- ezy_hr/hooks.py
+++ ezy_hr/hooks.py
@@ -144,7 +144,7 @@ fixtures = [
                     "Job Applicant-custom_reference_previous_second_company",
                     "Job Applicant-custom_reference_check_authorisation_letter",
                     "Job Applicant-custom_reference_check",
-                    "Job Applicant-custom_authorisation_letter"
+                    "Job Applicant-custom_authorisation_letter",
                     "Employee-custom_salary_breakdown",
                     "Employee-custom_earnings",
                     "Employee-custom_deductions"
