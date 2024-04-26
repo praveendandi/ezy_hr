8d81991785905cbdbd3c3ea3e395337be215df37
mail <mail@gmail.com>
2024-04-26 12:01:56 +0530

    employee_checkin_and_checkout_details

--- ezy_hr/ezy_hr/report/employee_checkin_and_checkout_details/employee_checkin_and_checkout_details.js
+++ ezy_hr/ezy_hr/report/employee_checkin_and_checkout_details/employee_checkin_and_checkout_details.js
@@ -2,48 +2,44 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Employee Checkin And Checkout Details"] = {
-	filters: [
-		{
-			fieldname: "from_date",
-			label: __("From Date"),
-			fieldtype: "Date",
-			reqd: 1,
-			default: frappe.datetime.month_start(),
-		},
-		{
-			fieldname: "to_date",
-			label: __("To Date"),
-			fieldtype: "Date",
-			reqd: 1,
-			default: frappe.datetime.month_end(),
-		},
-		{
-			fieldname: "employee",
-			label: __("Employee"),
-			fieldtype: "Link",
-			options: "Employee",
-		},
-		// {
-		// 	fieldname: "shift",
-		// 	label: __("Shift Type"),
-		// 	fieldtype: "Link",
-		// 	options: "Shift Type",
-		// },
-		// {
-		// 	fieldname: "department",
-		// 	label: __("Department"),
-		// 	fieldtype: "Link",
-		// 	options: "Department",
-		// },
-		// {
-		// 	fieldname: "company",
-		// 	label: __("Company"),
-		// 	fieldtype: "Link",
-		// 	options: "Company",
-		// 	reqd: 1,
-		// 	default: frappe.defaults.get_user_default("Company"),
-		// },
-		
-	],
-
+    filters: [
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.month_start(),
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.month_end(),
+        },
+        {
+            fieldname: "employee",
+            label: __("Employee"),
+            fieldtype: "Link",
+            options: "Employee"
+        },
+        {
+            fieldname: "company",
+            label: __("Unit"),
+            fieldtype: "Link",
+            options: "Company"
+        },
+    ],
+    
 };
+frappe.ui.form.on('Employee Checkin', {
+    onload: function(frm) {
+        if (frm.doc.__islocal) { // Check if the form is new
+            frm.set_value('log_type', frm.doc.log_type || 'IN'); // Set default value for log_type
+            frm.set_value('device_id', frm.doc.device_id || 'Manual'); // Set default value for device_id
+
+            frm.toggle_enable('log_type', 0); // Set log_type as read-only
+            frm.toggle_enable('device_id', 0); // Set device_id as read-only
+            frm.toggle_enable('employee', 0); // Set employee as read-only
+        }
+    }
+});
+
--- ezy_hr/ezy_hr/report/employee_checkin_and_checkout_details/employee_checkin_and_checkout_details.py
+++ ezy_hr/ezy_hr/report/employee_checkin_and_checkout_details/employee_checkin_and_checkout_details.py
@@ -1,30 +1,26 @@
-# Copyright (c) 2024, Ganu Reddy and contributors
-# For license information, please see license.txt
-
+# # Copyright (c) 2024, Ganu Reddy and contributors
+# # For license information, please see license.txt
 
 import frappe
 
-
 def execute(filters=None):
     columns = [
-        {"label": "Employee ID", "fieldname": "employee", "fieldtype": "Data", "width": 150},
+        {"label": "Employee ID", "fieldname": "employee", "fieldtype": "Link", "options": "Employee", "width": 150},
         {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 150},
+        {"label": "Unit", "fieldname": "company", "fieldtype": "Link", "options": "Company", "width": 150},
         {"label": "Department", "fieldname": "department", "fieldtype": "Link", "options": "Department", "text-align": "Center", "width": 180},
         {"label": "Date Of Joining", "fieldname": "date_of_joining", "fieldtype": "Date", "width": 150},
         {"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 150},
         {"label": "First Checkin", "fieldname": "in_time", "fieldtype": "Datetime", "width": 180},
         {"label": "Last Checkout", "fieldname": "out_time", "fieldtype": "Datetime", "width": 180},
-        {"label": "Total Working Hours", "fieldname": "working_hours", "fieldtype": "Data", "width": 180},
-        {"label": "Status", "fieldname": "status", "fieldtype": "Data", "text-align": "Center", "width": 180}
+        {"label": "Duration", "fieldname": "working_hours", "fieldtype": "Data", "width": 180},
+        {"label": "Status", "fieldname": "status", "fieldtype": "Data", "text-align": "Center", "width": 180},
+        {"label": "Actions", "fieldname": "actions", "fieldtype": "Button", "width": 100}
     ]
 
     data = get_data(filters)
     
     return columns, data
-
-
-
-
 
 def get_data(filters):
     conditions = []
@@ -33,16 +29,15 @@ def get_data(filters):
         conditions.append(f"date(ec.time) >= '{filters['from_date']}'")
     if filters.get("to_date"):
         conditions.append(f"date(ec.time) <= '{filters['to_date']}'")
+    if filters.get("employee"):
+        conditions.append(f"ec.employee = '{filters['employee']}'")
+    if filters.get("unit"):
+        conditions.append(f"e.unit = '{filters['unit']}'")
 
     condition_str = " AND ".join(conditions) if conditions else "1=1"
 
-    if filters.get("employee"):
-        employee_condition = f"ec.employee = '{filters['employee']}' AND "
-    else:
-        employee_condition = ""
-
     sql_query = f"""
-        SELECT ec.employee as employee, e.employee_name, e.date_of_joining, date(ec.time) as date,
+        SELECT ec.employee as employee, e.employee_name,e.company, e.date_of_joining, date(ec.time) as date,
                MIN(ec.time) as in_time,
                (
                    SELECT MIN(time)
@@ -83,6 +78,11 @@ def get_data(filters):
                REPLACE(e.department, ' - TPB', '') as department,
                e.company,
                CASE 
+                   WHEN (TIMESTAMPDIFF(MINUTE, MIN(ec.time), (
+                           SELECT MIN(time)
+                           FROM `tabEmployee Checkin`
+                           WHERE employee = ec.employee AND log_type = 'OUT' AND date(time) = date(ec.time)
+                       )) = 0) THEN ''
                    WHEN MIN(ec.time) IS NOT NULL AND 
                         (
                             SELECT MIN(time)
@@ -99,12 +99,28 @@ def get_data(filters):
                END as status
         FROM `tabEmployee Checkin` ec
         JOIN `tabEmployee` e ON ec.employee = e.name
-        WHERE {condition_str} AND {employee_condition}ec.time IS NOT NULL
+        WHERE {condition_str} AND ec.time IS NOT NULL
         GROUP BY ec.employee, e.employee_name, e.date_of_joining, date(ec.time), e.department, e.company
         ORDER BY MIN(ec.time) DESC
     """
 
     data = frappe.db.sql(sql_query, as_dict=1)
+    
+    for row in data:
+        if not row.get("in_time"):
+            row["log_type"] = "IN"
+            row["device_id"] = "Manual"
+        elif not row.get("out_time"):
+            row["log_type"] = "OUT"
+            row["device_id"] = "Manual"
+        else:
+            row["log_type"] = ""
+            row["device_id"] = ""
+
+        if not row.get("in_time") or not row.get("out_time"):
+            base_url = frappe.utils.get_url()
+            row["actions"] = f'<button class="btn btn-secondary btn-sm edit-btn" data-employee="{row["employee"]}" data-in-time="{row["in_time"]}" data-out-time="{row["out_time"]}" data-log-type="{row["log_type"]}" data-device-id="{row["device_id"]}"><a href="{base_url}/app/employee-checkin/new-employee-checkin?employee={row["employee"]}&log_type={row["log_type"]}&device_id=Manual">Edit</a></button>'
+        else:
+            row["actions"] = ''
 
     return data
-
