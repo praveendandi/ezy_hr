9a2bed6b5b911a8b9ba9ffc364401dcd024c9920
mail <mail@gmail.com>
2024-08-13 12:27:39 +0530

    Salary Tab Hidden in the Employee for all the users except those who have the Salary Slips role

---
+++ ezy_hr/public/js/salary_hide.js
@@ -0,0 +1,30 @@
+frappe.ui.form.on('Employee', {
+    refresh: function(frm) {
+        frappe.call({
+            method: 'ezy_hr.employee_salary.get_user_employee_id',
+            args: {
+                data: frm.doc
+            },
+            callback: function (value) {
+                if (value.message && value.message.length > 0) {
+                    let employee_id = value.message[0].for_value;
+                    if (employee_id === frm.doc.name || frappe.user.has_role(["System Manager", "HR Manager", "Salary Slips"])) {
+
+                        frm.set_df_property('custom_gross_amount', 'hidden', 0);
+                        frm.set_df_property('custom_earnings', 'hidden', 0);
+                        frm.set_df_property('custom_deductions', 'hidden', 0);
+                    } else {
+                        frm.set_df_property('custom_gross_amount', 'hidden', 1);
+                        frm.set_df_property('custom_earnings', 'hidden', 1);
+                        frm.set_df_property('custom_deductions', 'hidden', 1);
+                    }
+                } else {
+                    frm.set_df_property('custom_gross_amount', 'hidden', 1);
+                    frm.set_df_property('custom_earnings', 'hidden', 1);
+                    frm.set_df_property('custom_deductions', 'hidden', 1);
+                }
+            }
+        });
+    }
+});
+
