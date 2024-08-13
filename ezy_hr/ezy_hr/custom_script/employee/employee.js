e33ca12cd3b2365b29499b86abdea00363057b91
prasad <prasad.prasad@y>
2024-08-13 10:49:52 +0530

    Removed unwanted code

--- ezy_hr/ezy_hr/custom_script/employee/employee.js
+++ ezy_hr/ezy_hr/custom_script/employee/employee.js
@@ -6,39 +6,19 @@ frappe.ui.form.on('Employee', {
             };
             frappe.set_route('Form', 'Employee Fields Update','new_employee_fields_update');
         }).addClass('btn-primary');
- 
-        // Add the 'Create User' button if no user_id is set and employee status is not 'Left'
-        if (!frm.doc.user_id && frm.doc.employee && frm.doc.status != 'Left') {
-            frm.add_custom_button(__('Create User'), function() {
-                create_user_for_employee(frm);
-            }).addClass('btn-secondary');
-        }
- 
-       
-        if (frm.doc.status === 'Left' && frm.doc.user_id) {
-            frappe.call({
-                method: 'frappe.client.set_value',
-                args: {
-                    doctype: 'User',
-                    name: frm.doc.user_id, 
-                    fieldname: 'enabled',
-                    value: 0
-                },
-                callback: function(r) {
-                    if (!r.exc) {
-                        frm.set_value('user_id', null);
-                        frm.refresh();
-                    } else {
-                        frappe.msgprint(__('Error occurred while updating the user status'));
-                        frappe.log_error(r.exc);
-                    }
-                },
-                error: function(r) {
-                    frappe.msgprint(__('Error occurred while updating the user status'));
-                    frappe.log_error(r);
+    },
+    custom_leave_policy: function(frm) {       
+        frappe.call({
+            method: "ezy_hr.ezy_hr.custom_script.employee.employee.assign_leave_policy",
+            args: { doc: cur_frm.doc },
+            callback: function(r) {
+                if (r.message.success) {
+                    frappe.msgprint(r.message.reason);
+                } else {
+                    frappe.msgprint(r.message.reason);
                 }
-            });
-        }
+            }
+        });
     },
     reports_to: function(frm) {
         frappe.db.get_value("Employee", {"name": frm.doc.reports_to}, "user_id")
@@ -49,21 +29,5 @@ frappe.ui.form.on('Employee', {
                 frm.set_value('shift_request_approver', report_user_id);
             });
     }
- });
- 
- function create_user_for_employee(frm) {
-    frappe.call({
-        method: 'ezy_hr.ezy_hr.custom_script.employee.employee.create_employee_user',
-        args: {
-            doc : frm.doc.name
-        },
-        callback: function(r) {
-            if (!r.exc) {
-                frm.set_value('user_id', r.message);
-                frm.refresh();
-            } else {     
-                frappe.log_error(r.exc);
-            }
-        });
-    }
+
 });
