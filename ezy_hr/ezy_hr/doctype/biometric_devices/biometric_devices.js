f1da76862dfa9ef31f777fc3983c5b19dfa864a3
Nithin <nithinreddy@caratred.com>
2024-08-14 16:54:55 +0530

    change add cm house attendace files

---
+++ ezy_hr/ezy_hr/doctype/biometric_devices/biometric_devices.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ganu Reddy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Biometric Devices", {
+// 	refresh(frm) {
+
+// 	},
+// });
