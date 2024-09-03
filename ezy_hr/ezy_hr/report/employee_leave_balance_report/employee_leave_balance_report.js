450a306cb13ff7921dd3c045ffbdc8ac20b793f5
mail <mail@gmail.com>
2024-09-03 15:31:29 +0530

    Employee Leave Balance Doctype & Employee Leave Balance Report Created

---
+++ ezy_hr/ezy_hr/report/employee_leave_balance_report/employee_leave_balance_report.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2024, Ganu Reddy and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Employee Leave Balance Report"] = {
+    "filters": [
+        {
+            "fieldname": "unit",
+            "label": __("Unit"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_default("company")
+        },
+        {
+            "fieldname": "employee",
+            "label": __("Employee"),
+            "fieldtype": "Link",
+            "options": "Employee",
+            "default": ""
+        },
+        {
+            "fieldname": "leave_type",
+            "label": __("Leave Type"),
+            "fieldtype": "Link",
+            "options": "Leave Type",
+            "default": ""
+        }
+    ]
+};
