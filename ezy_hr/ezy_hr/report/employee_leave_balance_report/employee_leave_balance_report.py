9f96566246f6494df513231e5b8fe34827416972
mail <mail@gmail.com>
2024-09-06 12:50:25 +0530

    Added Used Leaves Count in the employee_leave_balance_report

--- ezy_hr/ezy_hr/report/employee_leave_balance_report/employee_leave_balance_report.py
+++ ezy_hr/ezy_hr/report/employee_leave_balance_report/employee_leave_balance_report.py
@@ -2,16 +2,15 @@
 # For license information, please see license.txt
 
 import frappe
-
 def execute(filters=None):
     columns = [
         {"label": "Employee", "fieldname": "employee", "fieldtype": "Data", "options": "Employee", "width": 150},
         {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 150},
-        {"label": "Leave Type", "fieldname": "leave_type", "fieldtype": "Link", "options": "Leave Type", "width": 150},
+        # {"label": "Leave Type", "fieldname": "leave_type", "fieldtype": "Data", "width": 150},
         {"label": "Leave Balance Given On", "fieldname": "leave_balance_on", "fieldtype": "Date", "width": 200},
         {"label": "Allocate Frequency", "fieldname": "allocate_frequency", "fieldtype": "Data", "width": 150},
         {"label": "Allocated Count", "fieldname": "allocated_count", "fieldtype": "Float", "width": 150},
-        {"label": "Leaves Updated On", "fieldname": "leave_balance_updated", "fieldtype": "Date", "width": 180},
+        {"label": "Leaves Updated On", "fieldname": "leave_balance_updated", "fieldtype": "Date", "width": 180}
     ]
 
     leave_type_abbr = {
@@ -33,28 +32,35 @@ def execute(filters=None):
         "Compensatory Off" : "CO"
     }
 
-    leave_types = []
-    if filters.get("leave_type"):
-        leave_types = [{"leave_type": filters.get("leave_type")}]
-    else:
-        leave_types = frappe.get_all("Employee Leave Balance", fields=["distinct leave_type"])
+    leave_types = frappe.get_all("Employee Leave Balance", fields=["employee", "employee_name", "leave_type", "leave_balance_on", "allocate_frequency", "allocated_count", "leave_balance_updated"])
 
-    for leave_type in leave_types:
-        leave_type_name = leave_type['leave_type']
-        abbr = leave_type_abbr.get(leave_type_name, leave_type_name[:2]) 
-        columns.append({"label": f"{abbr} Opening Balance", "fieldname": f"{abbr.lower()}_leave_balance", "fieldtype": "Float", "width": 180})
-        columns.append({"label": f"{abbr} Current Balance", "fieldname": f"{abbr.lower()}_current_balance", "fieldtype": "Float", "width": 180})
+    leave_type_filters = []
+    if filters.get("leave_type"):
+        leave_type_filters.append(filters.get("leave_type"))
+    else:
+        leave_type_filters = [leave['leave_type'] for leave in leave_types]
+
+    # Set to keep track of already added columns for leave types
+    added_leave_types = set()
+
+    for leave_type in leave_type_filters:
+        abbr = leave_type_abbr.get(leave_type, leave_type[:2])
+        if abbr not in added_leave_types:
+            # Add columns only if they haven't been added already
+            columns.append({"label": f"{abbr} Opening Balance", "fieldname": f"{abbr.lower()}_leave_balance", "fieldtype": "Float", "width": 180})
+            columns.append({"label": f"{abbr} Total Balance", "fieldname": f"{abbr.lower()}_current_balance", "fieldtype": "Float", "width": 180})
+            columns.append({"label": f"{abbr} Used Leaves", "fieldname": f"{abbr.lower()}_used_leaves", "fieldtype": "Float", "width": 180})
+            columns.append({"label": f"{abbr} Current Balance", "fieldname": f"{abbr.lower()}_adjusted_balance", "fieldtype": "Float", "width": 180})
+            added_leave_types.add(abbr)  # Mark this leave type as added
 
     conditions = {}
     if filters.get("unit"):
         conditions["unit"] = filters.get("unit")
     if filters.get("employee"):
         conditions["employee"] = filters.get("employee")
-    if filters.get("employee_name"):
-        conditions["employee_name"] = ['like', '%' + filters.get("employee_name") + '%']
     if filters.get("leave_type"):
         conditions["leave_type"] = filters.get("leave_type")
-    
+
     leave_balances = frappe.get_all(
         "Employee Leave Balance",
         filters=conditions,
@@ -63,29 +69,39 @@ def execute(filters=None):
         order_by="employee, leave_balance_on"
     )
 
-    data_dict = {}
+    used_leaves_data = frappe.get_all(
+        "Leave Application",
+        filters={"status": "Approved"},
+        fields=["employee", "leave_type", "sum(total_leave_days) as used_leaves"],
+        group_by="employee, leave_type"
+    )
+
+    used_leaves_map = {(row['employee'], row['leave_type']): row['used_leaves'] for row in used_leaves_data}
+
+    employee_data = {}
 
     for leave in leave_balances:
-        key = (leave['employee'], leave['leave_balance_on'])
+        emp_key = leave['employee']
         leave_type = leave['leave_type']
-        abbr = leave_type_abbr.get(leave_type, leave_type[:2]).lower()  
+        abbr = leave_type_abbr.get(leave_type, leave_type[:2]).lower()
 
-        if key not in data_dict:
-            data_dict[key] = {
+        if emp_key not in employee_data:
+            employee_data[emp_key] = {
                 "employee": leave["employee"],
                 "employee_name": leave["employee_name"],
-                "leave_type": leave["leave_type"],  
+                "leave_type": leave["leave_type"],  # Add leave_type here
                 "leave_balance_on": leave["leave_balance_on"],
                 "allocate_frequency": leave["allocate_frequency"],
                 "allocated_count": leave["allocated_count"],
-                "leave_balance": leave["leave_balance"],
                 "leave_balance_updated": leave["leave_balance_updated"],
-                "current_leave_balance": leave["current_leave_balance"]
             }
 
-        data_dict[key][f"{abbr}_leave_balance"] = leave["leave_balance"]
-        data_dict[key][f"{abbr}_current_balance"] = leave["current_leave_balance"]
+        employee_data[emp_key][f"{abbr}_leave_balance"] = leave["leave_balance"]
+        employee_data[emp_key][f"{abbr}_current_balance"] = leave["current_leave_balance"]
+        employee_data[emp_key][f"{abbr}_used_leaves"] = used_leaves_map.get((leave["employee"], leave["leave_type"]), 0)
+        adjusted_balance = leave["current_leave_balance"] - employee_data[emp_key][f"{abbr}_used_leaves"]
+        employee_data[emp_key][f"{abbr}_adjusted_balance"] = adjusted_balance
 
-    data = list(data_dict.values())
+    data = list(employee_data.values())
 
     return columns, data
